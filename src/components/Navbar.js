d75393e5fc8ace90693a0dd66d8f798e5e576a34
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-01-06 14:53:26 +0500

    completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
+// import pdf from "../Assets/Muhammad's Resume.pdf";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
@@ -92,7 +92,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="https://soumyajitblogs.vercel.app/"
                 target="_blank"
@@ -100,11 +100,11 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/mabdullahjs/"
                 target="_blank"
                 className="fork-btn-inner"
               >
--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -9,8 +9,10 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  // "https://github.com/mabdullahjs/Portfolio-Using-ReactJs/blob/3239a6c7a3d1b0d2860097b02fea995b2791f43a/src/Assets/Muhammad's%20Resume.pdf";
 
+  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+// "https://github.com/soumyajit4419/Portfolio/blob/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -20,6 +22,9 @@ function ResumeNew() {
 
   return (
     <div>
+      <a  href={pdf} download="pdf" target="_blank">
+        <Button style={{marginTop:"5rem"}}>My Example Doc</Button>
+      </a>
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
